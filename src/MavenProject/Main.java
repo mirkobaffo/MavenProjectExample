3891c0fc6da16b5b42e920ef3b3da89077398767
mirko <mirkoleandri110392@gmail.com>
2021-03-29 18:05:08 +0200

    Frozen commit

--- src/MavenProject/Main.java
+++ src/MavenProject/Main.java
@@ -3,7 +3,7 @@ package MavenProject;
 public class Main {
 	public static void main(String[] args) {
 
-	      System.out.println("Hello, World!");
+	      System.out.println("Hello, Glacial World!");
 
 	   }
 
